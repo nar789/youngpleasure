b05a4e05b1943e9260d598f3126dfdc39bce96bd
nar789 <admin@hume.co.kr>
2019-12-21 14:04:37 +0900

    add hook in route for login check

--- src/Route.js
+++ src/Route.js
@@ -6,14 +6,40 @@
 
 		function route(){
 			app.get('/',function(req,res){
-				res.render('index.html',{});
+				loginCheckRouteHook(()=>{
+					res.render('index.html',{});	
+				});
 			});
-
 			//1. enetry point
 			app.listen(1221,function(){
 			  preLoad();
 			  console.log('YoungPleasure! Server listen on *:1221');
 			});
+		}
+
+		function loginCheckRouteHook(doInLoginCheckRouteHook){	
+			routeHook(()=>{
+				return {result:"success"};
+			},(params)=>{
+				if(params==undefined || params.result==undefined){
+					return;
+				}
+				if(params.result === "success"){
+					//to-do-something
+					doInLoginCheckRouteHook();
+				} else { //in case of not having session, or not login..etc..
+					//to-do
+				}
+				return {result:"success"};
+			},(params)=>{
+				return 1;
+			});
+		}
+
+		function routeHook(onPreExecute,doInRoute,onPostExecute){
+			var preReturn = onPreExecute();
+			var doReturn = doInRoute(preReturn);
+			return onPostExecute(doReturn);
 		}
 
 		function preLoad(){
